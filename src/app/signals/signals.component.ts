73bc5cae07cc70421273f5266197236ca23fc304
Mona400 <monasallam400@gmail.com>
2023-11-17 16:40:20 +0200

    Signals

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, Signal, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   templateUrl: './signals.component.html',
   styleUrls: ['./signals.component.css']
 })
-export class SignalsComponent {
-   counter: number = 0;
+export class SignalsComponent implements DoCheck{
+   ngDoCheck(): void {
+    console.log("Some thing has been change")
+   }
+   counter=signal(0);
+   computecounter=computed(()=>this.counter()*2)//compute signal
 
-   message: string[] = [];
-
+   constructor(){
+    effect(()=>console.log('the current value '+this.counter()+ ''))//must use signal value to call effect function
+   }
+   //--change {message: string[] = []; }to signal 
+message=signal<string[]>([])
    increment(){
-    this.counter++;
-    this.message.push('Current counter value is: ' + this.counter);
+    //---set
+    //this.counter.set(this.counter()+1 );
+   //---update
+   this.counter.update((x)=>x+1)
+    //this.message.push('Current counter value is: ' + this.counter);
+   // this.message.update((m)=>[...m,'current value of counter is '+this.counter()+''])
+   this.message.mutate((m)=>m.push('current value of counter is '+this.counter()+''))
    }
 
    decrement(){
-    this.counter--;
-    this.message.pop();
+    //---set
+    //this.counter.set(this.counter()-1 );
+   //---update
+   this.counter.update((x)=>x-1)
+    //this.message.pop();
+    this.message.mutate((m)=>m.pop())
    }
 }
